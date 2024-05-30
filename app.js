a75d1002ee2e1487ae4e8f12b8978da24047cd52
sgkrish19 <gokulkrish1911@gmail.com>
2024-05-30 18:33:36 +0530

    adding in the cors at line 12

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:true, credentials:true}));
+// app.use(cors());
+
 
 const server = http.createServer(app);
 const io = socketIO(server, {
